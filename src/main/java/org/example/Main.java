431f74f2cd45a63a2fd42266bfa07a2bbee6c33a
Caden Epp <cadenepp@Cadens-MacBook-Air.local>
2024-11-21 10:59:50 -0600

    added random number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Caden Epp");
+        //System.out.println("Caden Epp");
+        //JOptionPane.showMessageDialog(null, "Hello World");
+        System.out.println("Your random number is " + (1 + (int) (Math.random() * 10)));
     }
 }
